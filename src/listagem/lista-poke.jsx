df08b91b691c21baa0b3cd522285e63671860eae
GuedesDev01 <guedesbruno1011@gmail.com>
2024-05-07 09:14:25 -0300

    alteração

---
+++ src/listagem/lista-poke.jsx
@@ -0,0 +1,18 @@
+
+const Listagem = ()=> 
+
+    (
+        <>
+        
+        <div>
+            <ul>
+                <li>pokemon 1</li>
+                <li>pokemon 2</li>
+                <li>pokemon 3</li>
+            </ul>
+        </div>
+    </>
+    )
+
+
+export default Listagem
